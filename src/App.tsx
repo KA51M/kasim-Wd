359b1866eb6dda519d09f1d9c1e0a49ef9e6f577
Kasim <kasimshaikh1713@gmail.com>
2026-07-30 14:47:09 +0530

    Remove video marquee section layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 
 import { HeroSection } from './components/sections/HeroSection';
-import { MarqueeSection } from './components/sections/MarqueeSection';
 import { AboutSection } from './components/sections/AboutSection';
 import { ServicesSection } from './components/sections/ServicesSection';
 import { ProjectsSection } from './components/sections/ProjectsSection';
@@ -9,7 +8,6 @@ function App() {
   return (
     <main className="w-full bg-[#0C0C0C] min-h-screen overflow-x-clip text-white selection:bg-white/20">
       <HeroSection />
-      <MarqueeSection />
       <AboutSection />
       <ServicesSection />
       <ProjectsSection />
